8a9b066042a33f4c409eeff7995be76e1b36ea05
waghvishalg <waghvishalg@gmail.com>
2023-08-02 07:27:20 +0530

    Initial Commit

---
+++ H4XOR/H4XORApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORApp.swift
+//  H4XOR
+//
+//  Created by Vishal Wagh on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct H4XORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
